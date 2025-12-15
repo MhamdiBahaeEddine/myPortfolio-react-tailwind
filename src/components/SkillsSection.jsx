bdefc07fe1330e6c24eb87b3c12c5e5f022f2152
Bahae Eddine Mhamdi <bahaeeddine120@gmail.com>
2025-12-15 20:33:35 +0100

    fix responsive

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -31,7 +31,7 @@ export const SkillSection = () => {
   );
 
   return (
-    <section id="skills" className="py-24 px-60 bg-secondary/30">
+    <section id="skills" className="py-24 px-[10%] bg-secondary/30">
       <div className="container mx-auto max-w-5xl">
         <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
           My <span className="text-primary">Skills</span>
