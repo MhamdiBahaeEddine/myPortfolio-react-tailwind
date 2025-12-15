35f365116dab5309d08f938d78c4e2b3d80302eb
Bahae Eddine Mhamdi <bahaeeddine120@gmail.com>
2025-12-15 15:02:42 +0100

    fix error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Header from "./components/header"
 import { Home } from "./pages/home"
 import { NotFound } from "./pages/NotFound"
 import { Toaster } from "@/components/ui/toaster"
